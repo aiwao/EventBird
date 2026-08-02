0e524f1e00c2b5a41a26f81259bbe12569c1e162
aiwao <233898056+aiwao@users.noreply.github.com>
2026-08-02 17:24:54 +0900

    publish on the maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     kotlin("jvm") version "2.4.0"
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
-group = "com.github.aiwao"
+group = "io.github.aiwao"
 version = providers.gradleProperty("eventBirdVersion")
     .getOrElse("1.0-SNAPSHOT")
 
@@ -21,14 +21,46 @@ kotlin {
     jvmToolchain(25)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("githubPackages") {
-            from(components["java"])
-            artifactId = "eventbird"
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(group.toString(), "eventbird", version.toString())
+
+    pom {
+        name.set("EventBird")
+        description.set("A lightweight annotation-based event bus for Kotlin/JVM.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/aiwao/EventBird")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("aiwao")
+                name.set("aiwao")
+                email.set("aiwao@users.noreply.github.com")
+                url.set("https://github.com/aiwao")
+                organization.set("aiwao")
+                organizationUrl.set("https://github.com/aiwao")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/aiwao/EventBird")
+            connection.set("scm:git:git://github.com/aiwao/EventBird.git")
+            developerConnection.set("scm:git:ssh://git@github.com/aiwao/EventBird.git")
         }
     }
+}
 
+publishing {
     repositories {
         maven {
             name = "GitHubPackages"
